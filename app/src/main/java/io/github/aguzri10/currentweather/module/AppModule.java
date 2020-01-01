3728fb77137e7bc7c37e5ea8963e5661037679d3
Aguz Riyanto <aguzri10@gmail.com>
2020-01-01 13:45:29 +0700

    final commit

--- app/src/main/java/io/github/aguzri10/currentweather/module/AppModule.java
+++ app/src/main/java/io/github/aguzri10/currentweather/module/AppModule.java
@@ -8,7 +8,7 @@ public class AppModule {
     private static final String BASE_URL = "https://openweathermap.org/data/2.5/";
     public static final String URL_IMAGE = "http://openweathermap.org/img/wn/";
     public static final String URL_IMAGE_EX = "@2x.png";
-    public static final String appid = "b6907d289e10d714a6e88b30761fae22";
+    public static final String appid = "<YOUR_APPID_OPENWEATHERMAP>";
     private static Retrofit retrofit = null;
 
     public AppModule(Retrofit retrofit) {
